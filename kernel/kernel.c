ef0618fc66a258e4d43d048237a8d3b609add9c8
Zonotora <zonotora@hotmail.com>
2024-06-08 01:23:30 +0200

    Interrupts

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,18 +1,15 @@
 #include "../drivers/ports.h"
 #include "../drivers/screen.h"
 #include "utils.h"
+#include "../cpu/idt.h"
+#include "../cpu/isr.h"
 
 void main() {
     clear_screen();
 
-    /* Fill up the screen */
-    int i = 0;
-    for (i = 0; i < 24; i++) {
-        char str[255];
-        int_to_ascii(i, str);
-        kprint_at(str, i, 0);
-    }
 
-    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 24, 60);
-    kprint("And with this text, the kernel will scroll again, and row 1 will disappear too!");
+    isr_install();
+    /* Test the interrupts */
+    __asm__ __volatile__("int $2");
+    __asm__ __volatile__("int $3");
 }
